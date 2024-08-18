0c120897c0ffcae038f3608346e6e0f0de00922f
Toshiyuki Masui <masui@pitecan.com>
2024-08-18 16:03:38 +0900

    ...

--- perfect.js
+++ perfect.js
@@ -24,12 +24,10 @@ function randomNotes(n){
     // シャッフル
     for(i = a.length - 1; i > 0; i--){
 	var r = Math.floor(Math.random() * (i + 1))
-	[a[i], a[r]] = [a[r], a[i]]
-	/*
+	//[a[i], a[r]] = [a[r], a[i]]
 	var tmp = a[i]
 	a[i] = a[r]
 	a[r] = tmp
-	*/
     }
     return a.slice(0,n).sort()
 }
