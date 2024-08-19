5417d7ee841e8bb586ebb33c86e9ff36e62bd422
Toshiyuki Masui <masui@pitecan.com>
2024-08-20 07:59:15 +0900

    ...

--- perfect.js
+++ perfect.js
@@ -76,7 +76,7 @@ function play(){
     //alert(`play: ${instrument}`)
     //var inst = soundFont.instrument(instrument)
     //var inst = soundFont.instrument('flute')
-    soundFont.instrument(ctx, instrument).then(function (inst) {
+    Soundfont.instrument(ctx, instrument).then(function (inst) {
 	var time = ctx.currentTime + 0.1
 	inst.play(playNotes[0], time, 2.0)
 	inst.play(playNotes[1], time, 2.0)
